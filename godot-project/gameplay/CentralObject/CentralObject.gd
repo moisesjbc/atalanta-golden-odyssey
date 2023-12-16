99d1a4ae2f25341afd87b6def57e0dc24e2157ae
XxByJotaPexX <153957812+XxByJotaPexX@users.noreply.github.com>
2023-12-16 12:04:46 +0100

    Objeto central vida y daño de enemigo

--- godot-project/gameplay/CentralObject/CentralObject.gd
+++ godot-project/gameplay/CentralObject/CentralObject.gd
@@ -1,5 +1,30 @@
 extends Node2D
 
+export var max_health: float = 100.0
+var current_health: float = max_health
+
+# onready var health_bar: TextureProgress = $HealthBar  # Asigna la barra de vida desde el editor
+
 # Called when the node enters the scene tree for the first time.
-func _process(delta):
-	pass
+func _ready():
+	# Conectar la señal body_entered a la función _on_body_entered
+	connect("body_entered", self, "_on_body_entered")
+
+# Función llamada cuando el cuerpo (enemigo) entra en colisión con este objeto central
+func _on_body_entered(body):
+	if body.is_in_group("enemy"):  # Asegúrate de que el cuerpo que entra pertenezca al grupo de enemigos
+		# Reducir la salud del objeto central
+		take_damage(10.0)  # Puedes ajustar la cantidad de daño aquí
+
+# Función para reducir la salud del objeto central
+func take_damage(amount: float):
+	current_health -= amount
+	current_health = clamp(current_health, 0.0, max_health)  # Asegurarse de que la salud no sea menor que 0 ni mayor que max_health
+	print(current_health)
+	update_health_bar()
+
+# Función para actualizar la barra de vida# Función para actualizar la barra de vida
+func update_health_bar():
+	#health_bar.value = current_health
+	if current_health <= 0:
+		queue_free()  # Destruir el objeto central si la salud llega a cero
--- godot-project/gameplay/Enemy/Enemy1.gd
+++ godot-project/gameplay/Enemy/Enemy1.gd
@@ -3,10 +3,17 @@ extends KinematicBody2D
 export var speed: int = 500
 
 var targetposition = Vector2(960,540)
-
+var central_object
 # Called when the node enters the scene tree for the first time.
 func _process(delta):
 
 	var direction = (targetposition - global_position).normalized()
 	
-	move_and_collide(speed * direction * delta)
+	var collition = move_and_collide(speed * direction * delta)
+	if $Timer.is_stopped() and collition:
+		central_object = collition.collider
+		$Timer.start(1)
+
+
+func _on_Timer_timeout():
+	if central_object : central_object.take_damage(10.0)
