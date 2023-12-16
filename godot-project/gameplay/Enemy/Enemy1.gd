4990b9ac0584b81360fca905bac97ffa6817a24f
Moisés <moisesjbc@gmail.com>
2023-12-16 21:30:36 +0000

    Add ghost enemy

--- godot-project/gameplay/Enemy/Enemy1.gd
+++ godot-project/gameplay/Enemy/Enemy1.gd
@@ -9,10 +9,15 @@ var health = 3
 signal enemy_died
 var attacking = false
 var initial_h_scale
+var animated_sprite
 
 
 func _ready():
-	$animated_sprite.play("walking")
+	var selected_animation_index = randi() % $animations.get_child_count()
+	for animation_index in $animations.get_child_count():
+		$animations.get_child(animation_index).visible = (animation_index == selected_animation_index)
+	animated_sprite = $animations.get_child(selected_animation_index)
+	animated_sprite.play("walking")
 	initial_h_scale = scale.x
 
 
@@ -37,9 +42,9 @@ func _process(delta):
 
 	# Enemy looks to the left by default
 	if direction.x >= 0:
-		$animated_sprite.flip_h = true
+		animated_sprite.flip_h = true
 	
-	if $animated_sprite.animation != "dying":
+	if animated_sprite.animation != "dying":
 		var collition = move_and_collide(speed * direction * delta)
 		if not attacking and collition:
 			attacking = true
@@ -47,7 +52,7 @@ func _process(delta):
 
 func _on_Timer_timeout():
 	if is_instance_valid(central_object):
-		$animated_sprite.play("attacking")
+		animated_sprite.play("attacking")
 		$Timer.start(1)
 	else:
 		$Timer.stop()
@@ -58,26 +63,34 @@ func apply_damage(damage):
 	if health <= 0:
 		enemy_die()
 	else:
-		$animated_sprite.play("hurt")
+		animated_sprite.play("hurt")
 
 func enemy_die():
 	$Timer.stop()
 	$CollisionShape2D.disabled = true
-	$animated_sprite.play("dying")
+	animated_sprite.play("dying")
 
 
 func _on_animated_sprite_animation_finished():
-	print("Finished ", $animated_sprite.animation)
-	if $animated_sprite.animation == "attacking":
+	print("Finished ", animated_sprite.animation)
+	if animated_sprite.animation == "attacking":
 		central_object.take_damage(10.0)
-		$animated_sprite.play("idle")
-	elif $animated_sprite.animation == "dying":
+		animated_sprite.play("idle")
+	elif animated_sprite.animation == "dying":
 		print("Dying!")
 		emit_signal("enemy_died")
 		queue_free()
-	elif $animated_sprite.animation == "hurt":
+	elif animated_sprite.animation == "hurt":
 		if central_object:
-			$animated_sprite.play("idle")
+			animated_sprite.play("idle")
 		else:
-			$animated_sprite.play("walking")
+			animated_sprite.play("walking")
 		
+
+
+func _on_ghost_animation_finished():
+	_on_animated_sprite_animation_finished()
+
+
+func _on_skeleton_animation_finished():
+	_on_animated_sprite_animation_finished()
