7fb4398949ead3f813dd118db277f2c115348f35
XxByJotaPexX <153957812+XxByJotaPexX@users.noreply.github.com>
2023-12-16 15:38:38 +0100

    add damage to bullets

--- godot-project/gameplay/Enemy/Enemy1.gd
+++ godot-project/gameplay/Enemy/Enemy1.gd
@@ -4,6 +4,7 @@ export var speed: int = 500
 
 var targetposition = Vector2(960,540)
 var central_object
+var health = 3
 
 # Called when the node enters the scene tree for the first time.
 func _process(delta):
@@ -14,9 +15,16 @@ func _process(delta):
 		central_object = collition.collider
 		$Timer.start(1)
 
-
 func _on_Timer_timeout():
 	if is_instance_valid(central_object):
 		central_object.take_damage(10.0)
 	else:
 		$Timer.stop()
+
+func apply_damage(damage):
+	health -= damage
+	if health <= 0:
+		enemy_die()
+
+func enemy_die():
+	queue_free()
--- godot-project/gameplay/bullet/bullet.gd
+++ godot-project/gameplay/bullet/bullet.gd
@@ -7,7 +7,7 @@ extends KinematicBody2D
 var speed: int = 1500
 var target_position = Vector2.ZERO
 var velocity: Vector2 = Vector2.ZERO
-
+var damage: int = 1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,9 +17,10 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var collision = move_and_collide(speed * velocity * delta)
-	if collision:
-		collision.collider.queue_free()
-
+	if is_instance_valid(collision):
+		collision.collider.apply_damage(damage)
+		queue_free()
+	
 
 func _on_visibility_notifier_viewport_exited(viewport):
 	queue_free()
