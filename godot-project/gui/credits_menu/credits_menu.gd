ae600e809b3365c4ca5aa9c375da23d16ce5bd39
Moisés <moisesjbc@gmail.com>
2023-12-17 13:24:58 +0000

    Add menus flow

---
+++ godot-project/gui/credits_menu/credits_menu.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+
+func _on_play_pressed():
+	get_tree().change_scene("res://gameplay/main_stable/main_stable.tscn")
--- godot-project/gui/main_menu/main_menu.gd
+++ godot-project/gui/main_menu/main_menu.gd
@@ -1,5 +1,15 @@
 extends Node2D
 
 
+
+
+func _on_return_to_main_menu_pressed():
+	get_tree().change_scene("res://gui/main_menu/main_menu.tscn")
+
+
+func _on_credits_pressed():
+	get_tree().change_scene("res://gui/credits_menu/credits_menu.tscn")
+
+
 func _on_play_pressed():
 	get_tree().change_scene("res://gameplay/main_stable/main_stable.tscn")
